08d1b3981543e0016387d5e8f359131769e860b2
b1endong <tranhoanghai260805@gmail.com>
2025-09-03 17:30:22 +0700

    create navabar UI

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+export default function HomePage() {
+    return (
+        <>
+            <Navbar />
+            <div className="max-w-calc m-auto">
+                <h1>Home Page</h1>
+            </div>
+        </>
+    );
+}
